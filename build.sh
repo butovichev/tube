f67f63c833719ac1b3519b340e182617b0410bc2
butovichev <butovichev@yandex.ru>
2017-08-03 15:37:36 +0300

    first commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+
+
+sudo apt-get -y  install nodejs
+sudo apt-get -y  install npm
+npm install gulp --save-dev
+npm install gulp-ruby-sass \
+	 gulp-autoprefixer \
+	 gulp-cssnano \
+	 gulp-jshint \
+	 gulp-concat \
+	 gulp-uglify \
+	 gulp-imagemin \
+	 gulp-notify \
+	 gulp-rename \
+	 gulp-livereload \
+	 gulp-cache del \
+	 --save-dev
