8cacb6c98c72740c297661d501455d4094005ad4
Konstantin Korepin <korepinwork@gmail.com>
2017-09-27 19:53:21 +0400

    edit menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-var a = 1
+// ========== TABLE OF CONTENTS =========== //
+//
+// 1. Page Loader 
+// 2. Text fitting for headlines
+// 3. Parallax
+// 4. Calendar
+//
+// ======================================= //
+
+
+(function($) {
+    "use strict";
+	
+	/* 1. Page Loader */	
+	$(".loader-item").delay(7000).fadeOut();
+	$("#pageloader").delay(1200).fadeOut("slow");
+
+
+	
+
+})(jQuery);
+
